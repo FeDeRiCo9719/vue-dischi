47f7c4667ba7a90495908b31a3d4117f324e82f8
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-10-03 19:00:59 +0200

    base structure

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">    
+    <Header/>
+    <Albums/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Albums from './components/Albums.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Albums
   }
 }
 </script>
